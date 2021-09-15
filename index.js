5474ee249c3e6555beebccfb7b74ab2cd686834b
sagov8 <sago.vivas@gmail.com>
2021-09-14 19:25:15 -0500

    fecha add

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/day', (req, res) => {
+    days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    date = new Date()
+    res.send('Day is ${days[date.getDay() ]}')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
